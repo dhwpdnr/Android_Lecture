d06953fc1cfcf6fc0f78f5ec29008acbeabba0ae
dhwpdnr <ink1237@gmail.com>
2023-10-26 00:25:52 +0900

    [feat] animal select app

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,27 +2,76 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.RadioButton;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text1, text2;
+    CheckBox chkAgree;
+    RadioGroup rGroup;
+    RadioButton rdoDog, rdoCat, rdoRabbit;
+    Button btnOk;
+    ImageView imgPet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv1, tv2, tv3;
-        tv1 = (TextView) findViewById(R.id.TextView1);
-        tv2 = (TextView) findViewById(R.id.TextView2);
-        tv3 = (TextView) findViewById(R.id.TextView3);
+        setTitle("애완동물 사진 보기");
 
-        tv1.setText("안녕하세요 ?");
-        tv1.setTextColor(Color.RED);
-        tv2.setTextSize(30);
-        tv2.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD_ITALIC);
-        tv3.setText("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        tv3.setSingleLine();
+        text1 = (TextView) findViewById(R.id.Text1);
+        chkAgree = (CheckBox) findViewById(R.id.ChkAgree);
+
+        text2 = (TextView) findViewById(R.id.Text2);
+        rGroup = (RadioGroup) findViewById(R.id.Rgroup1);
+        rdoDog = (RadioButton) findViewById(R.id.RdoDog);
+        rdoCat = (RadioButton) findViewById(R.id.RdoCat);
+        rdoRabbit = (RadioButton) findViewById(R.id.RdoRabbit);
+
+        btnOk = (Button) findViewById(R.id.OkBtn);
+        imgPet = (ImageView) findViewById(R.id.ImgPet);
+
+        chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (chkAgree.isChecked() == true) {
+                    text2.setVisibility(View.VISIBLE);
+                    rGroup.setVisibility(View.VISIBLE);
+                    btnOk.setVisibility(View.VISIBLE);
+                    imgPet.setVisibility(View.VISIBLE);
+                } else {
+                    text2.setVisibility(View.INVISIBLE);
+                    rGroup.setVisibility(View.INVISIBLE);
+                    btnOk.setVisibility(View.INVISIBLE);
+                    imgPet.setVisibility(View.INVISIBLE);
+                }
+
+                btnOk.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int checkedRadioButtonId = rGroup.getCheckedRadioButtonId();
+                        if (checkedRadioButtonId == R.id.RdoDog) {
+                            imgPet.setImageResource(R.drawable.dog);
+                        } else if (checkedRadioButtonId == R.id.RdoCat) {
+                            imgPet.setImageResource(R.drawable.cat);
+                        } else if (checkedRadioButtonId == R.id.RdoRabbit) {
+                            imgPet.setImageResource(R.drawable.rabbit);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "동물을 먼저 선택하세요", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
