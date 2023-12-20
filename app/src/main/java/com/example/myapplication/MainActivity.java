6ed4edca219953a79fa0f188c41c25c6570de101
dhwpdnr <ink1237@gmail.com>
2023-12-20 18:47:58 +0900

    [feat] activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,89 +1,28 @@
 package com.example.myapplication;
 
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    DatePicker dp;
-    EditText edtDiary;
-    Button btnWrite;
-    String fileName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("간단 일기장");
-
-        dp = (DatePicker) findViewById(R.id.datePicker1);
-        edtDiary = (EditText) findViewById(R.id.edtDiary);
-        btnWrite = (Button) findViewById(R.id.btnWrite);
-
-        Calendar cal = Calendar.getInstance();
-        int cYear = cal.get(Calendar.YEAR);
-        int cMonth = cal.get(Calendar.MONTH);
-        int cDay = cal.get(Calendar.DAY_OF_MONTH);
-
-        dp.init(cYear, cMonth, cDay, new DatePicker.OnDateChangedListener() {
-            @Override
-            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                fileName = Integer.toString(year) + "-" + Integer.toString(monthOfYear) + "-" + Integer.toString(dayOfMonth) + ".txt";
-                String str = readDiary(fileName);
-                edtDiary.setText(str);
-                btnWrite.setEnabled(true);
-            }
-        });
-        btnWrite.setOnClickListener(new View.OnClickListener() {
+        Button btnNewActivity = (Button) findViewById(R.id.btnNewActivity);
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    FileOutputStream outFs = openFileOutput(fileName, Context.MODE_PRIVATE);
-                    String str = edtDiary.getText().toString();
-                } catch (IOException e) {
-
-                }
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
             }
         });
     }
-
-    String readDiary(String fName) {
-        String diaryStr = null;
-        FileInputStream inFs;
-        try {
-            inFs = openFileInput(fName);
-            byte[] txt = new byte[inFs.available()];
-            inFs.read(txt);
-            inFs.close();
-            diaryStr = (new String(txt)).trim();
-            btnWrite.setText("수정하기");
-        } catch (IOException e) {
-            edtDiary.setHint("일기 없음");
-            btnWrite.setText("새로 저장");
-        }
-        return diaryStr;
-    }
 }
+
